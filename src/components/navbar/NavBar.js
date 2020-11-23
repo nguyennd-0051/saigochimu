0e91bbfc3e11a2b919468533f8ada3c93fbe95d4
piapip <thovinh9997@gmail.com>
2020-11-23 20:32:08 +0700

    Fix search bar.

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Menu } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import SearchBar from '../searchBar/searchBar'
 // import { Link } from "react-router-dom";
 
 import {
@@ -11,10 +12,27 @@ import './NavBar.css';
 class NavBar extends Component {
 	render() {
 		return (
-			<Menu theme="light" selectedKeys={[]} mode="horizontal" style={{ position: 'fixed', zIndex: 1, width: '100%', height: 50 }}>
-				<Menu.Item key="index"><a href="/"><DingtalkOutlined />iDate</a></Menu.Item>
-				<Menu.Item key="profile" style={{float: 'right'}}><a href="/myplaces"><UserOutlined />My Profile</a></Menu.Item>
-			</Menu>
+			<div>
+				<Menu theme="light" selectedKeys={[]} mode="horizontal" style={{ position: 'fixed', zIndex: 1, width: '100%', height: 50 }}>
+					<Menu.Item key="index"><a href="/"><DingtalkOutlined />iDate</a></Menu.Item>
+					<Menu.Item key="profile" style={{float: 'right'}}><a href="/myplaces"><UserOutlined />My Profile</a></Menu.Item>
+				</Menu>
+				<div className="search-bar" style={{
+					width:"50%", 
+					margin: "auto", 
+					marginBottom: "2em",
+					position: "fixed",
+					"z-index": "99999",
+					top: "0.5%",
+					left: "25%" }}>
+					<SearchBar 
+						value={this.props.query}
+						handleSearch={this.props.handleSearch}
+					>
+					</SearchBar>
+				</div>
+			</div>
+			
 		);
 	}
 }
